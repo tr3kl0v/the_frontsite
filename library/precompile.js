c826dce034489be23a1b307c1f9bcb8c2dab76ef
Volkert Paap <hello@paapster.nl>
2013-10-22 11:59:04 +0200

    Added files.
    Signed-off-by: Volkert Paap <hello@paapster.nl>

---
+++ library/precompile.js
@@ -0,0 +1,18 @@
+var scriptToRun = process.argv[process.argv.length-1],
+	path = require('path');
+if(scriptToRun == 'modules'){
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileModules.js'));
+} else if (scriptToRun == 'components') {
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileComponents.js'));
+} else if (scriptToRun == 'pages') {
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compilePages.js'));
+} else if (scriptToRun == 'subs') {
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileSubModules.js'));
+} else if (scriptToRun == 'all'){
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileModules.js'));
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileComponents.js'));
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compilePages.js'));
+	require(path.join(__dirname, 'node_modules', 'compile-dust', 'bin', 'compileSubModules.js'));
+} else {
+	console.log("Error: Incorrect compile value. Either use 'node precompile pages' or 'node precompile modules'");
+}
---
+++ library/server.js
@@ -0,0 +1,29 @@
+// startup arguments
+var args = process.argv.splice(2).toString();
+
+// Dependencies
+var walk = require('walkdir'),
+	path = require('path'),
+
+// Print Logo
+logo = require('./routes/logo');
+logo.print();
+
+
+if (args == "all"){
+
+	var configFolder = path.join(__dirname, '../')
+
+	walk(configFolder, function(filepath, stat) {
+		if (filepath.indexOf(".svn") === -1 && filepath.indexOf("library") === -1) {
+
+			if(filepath.indexOf('app.js') > -1){
+				require(filepath)
+			}
+		}
+	})
+} else if (args == 'tests') {
+	require('./tests/app');
+} else {
+	var product = require('../' + args + '/server/app');
+}
