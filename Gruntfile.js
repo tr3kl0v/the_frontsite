c087c80cdaa7bb313c3c7aa9f166cc81224b5c2b
Volkert Paap <hello@paapster.nl>
2013-12-31 08:49:52 +0100

    Fixes: copy by Grunt of Bower_components.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,75 @@
 module.exports = function(grunt) {
 	'use strict';
-	
+
+	require('load-grunt-tasks')(grunt);
+
+	// Config variables
+	var configVars = {
+		"build": "build",
+		"dist_www": "./public",
+		"bower_soure": "./bower_components",
+		"dist_bower": "./public/static/js/vendor"
+	};
+
+
 	// Project configuration.
 	grunt.initConfig({
+
 		pkg: grunt.file.readJSON('package.json'),
+		cvars: configVars,
+		
 		bower: {
-			install: {
+			setup: {
 				options: {
-					targetDir: './public/static/js/vendor',
-					layout: 'byType',
 					install: true,
-					verbose: true,
-					cleanTargetDir: false,
-					cleanBowerDir: false,
-					bowerOptions: {}
+					copy: false
 				}
 			}
 		},
+
+	   copy: {
+			 "dist-bower" : {
+				files: [
+					{
+						src: '<%= cvars.bower_soure %>/jquery/jquery.min.map',
+						dest: "<%= cvars.dist_bower %>/jquery.min.map"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/jquery/jquery.min.js',
+						dest: "<%= cvars.dist_bower %>/jquery.min.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/lodash/dist/lodash.min.js',
+						dest: "<%= cvars.dist_bower %>/lodash.min.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/backbone/backbone-min.map',
+						dest: "<%= cvars.dist_bower %>/backbone.min.map"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/backbone/backbone-min.js',
+						dest: "<%= cvars.dist_bower %>/backbone.min.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/requirejs/require.js',
+						dest: "<%= cvars.dist_bower %>/require.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/modernizr/modernizr.js',
+						dest: "<%= cvars.dist_bower %>/modernizr.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/dustjs-linkedin/dist/dust-core-2.2.3.min.js',
+						dest: "<%= cvars.dist_bower %>/dust.core.min.js"
+					},
+					{
+						src: '<%= cvars.bower_soure %>/dustjs-linkedin-helpers/dist/dust-helpers-1.1.1.js',
+						dest: "<%= cvars.dist_bower %>/dust.helpers.js"
+					}
+				]
+			}
+		},
+		
 		watch: {
 			scss: {
 				files: ['lib/templates/modules/**/*.scss', 'lib/templates/submodules/**/*.scss'],
@@ -47,19 +100,21 @@ module.exports = function(grunt) {
 				tasks: ['jshint:utils']
 			}
 		},
-		jasmine : {
-			website1: {
-				src : ['/static/js/frontside.js', '/templates/modules/website1/**/*.js'],
-				options: {
-					specs: ['specs/general/*.spec.js', 'specs/website1/*.spec.js'],
-					host: 'http://127.0.0.1:8000/',
-					template: require('grunt-template-jasmine-requirejs'),
-					templateOptions: {
-						requireConfigFile:'specs/scripts.js'
-					}
-				}
-			}
-		},
+
+
+//		jasmine : {
+//			website1: {
+//				src : ['/static/js/frontside.js', '/templates/modules/website1/**/*.js'],
+//				options: {
+//					specs: ['specs/general/*.spec.js', 'specs/website1/*.spec.js'],
+//					host: 'http://127.0.0.1:8000/',
+//					template: require('grunt-template-jasmine-requirejs'),
+//					templateOptions: {
+//						requireConfigFile:'specs/scripts.js'
+//					}
+//				}
+//			}
+//		},
 		jshint: {
 			all: ['lib/templates/modules/**/*.js', 'lib/templates/submodules/**/*.js', 'static/js/extend.js', 'static/js/render.js'],
 			modules: ['lib/templates/modules/**/*.js'],
@@ -90,7 +145,7 @@ module.exports = function(grunt) {
 					basePath: '../website1/static/',
 					sassDir: 'sass',
 					cssDir: 'css',
-					fontsDir: '../../library/static/fonts',
+					fontsDir: '../website1/static/fonts',
 					environment: 'development'
 				}
 			}
@@ -107,20 +162,17 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('test', ['jasmine']);
+	//grunt.registerTask('test', ['jasmine']);
+	
 	
 	// Default task(s).
-	grunt.registerTask(
-		'default', [
-			'bower',
-			'watch',
-			'compass',
-			'jshint',
-			'dust',
-			'exec',
-			'concat',
-			'test'
-		]
-	);
+	grunt.registerTask('default', ['watch', 'compass', 'jshint', 'dust', 'exec', 'concat']);
+
+	/* BASIC TASKS */
+	grunt.registerTask('setup', ['bower:setup']);
+
+	/* Update bower repository -- must run build manually before this */
+	grunt.registerTask('dist-bower', ['copy:dist-bower']);
+
 };
 
