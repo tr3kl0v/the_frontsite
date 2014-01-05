f10512f7872f354d44e4fe182e285cbc50cd7b8b
Volkert Paap <hello@paapster.nl>
2014-01-05 16:09:29 +0100

    Path fixes.

--- server.js
+++ server.js
@@ -20,6 +20,8 @@
 │   See the License for the specific language governing permissions and       │
 │   limitations under the License.                                            │
 \*───────────────────────────────────────────────────────────────────────────*/
+'use strict';
+
 
 // startup arguments
 var args = process.argv.splice(2).toString();
