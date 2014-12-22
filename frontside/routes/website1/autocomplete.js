08b514f5cc0e7d3690e5df563a586460d270e172
Volkert Paap <hello@paapster.nl>
2014-12-22 14:30:37 +0100

    * Reordering structure * Removing not used test folder.

---
+++ frontside/routes/website1/autocomplete.js
@@ -0,0 +1,28 @@
+var request = require("../../../node_modules/request/index");
+
+exports.json = json;
+
+function json(req, res) {
+	var reqOptions = {
+		uri: 'http://www.test.com/autocomplete?q=' + req.params['id'] + '&limit=' + req.params['limit'] + '&timestamp=' + new Date().getTime() + '&module=EmtreeTool&autocomplete=y',
+		method: "GET"
+	}
+	//groupName=dna&groupType=WORD
+	request(reqOptions,
+		function(error, response, body) {
+			if(response.statusCode === 200){
+				try {
+					var json = JSON.parse(body);
+				} catch (e) {
+					console.log("Error with Autocomplete" + e)
+					var json = {};
+					json.description = "There is a problem with the accessing the data"
+				}
+			} else {
+				var json = {};
+				json.description = "Emtree Currently not available"
+			}
+			res.json(json)
+		}
+	);
+}
---
+++ frontside/server/website1/routes/filters.js
@@ -0,0 +1,17 @@
+var path = require('path'),
+	_ = require('../../../../node_modules/underscore/underscore');
+
+exports.json = json;
+exports.search = search;
+
+function json(req,res){
+	var json = require(path.join(req.app.get('json'), req.params['language'], 'filters.json')),
+		activeFilters = require(path.join(req.app.get('json'), 'activeFilters.json'));
+	json.activeFilters = activeFilters;
+	res.json(json)
+};
+
+function search(req, res){
+	var json = require(path.join(req.app.get('json'), 'filters.json'))[req.params.id];
+	res.json(json)
+};
